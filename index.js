bd1de46eed4b39a0e44e4dcd14f4a025bad85293
Tayyeba0 <159903055+Tayyeba0@users.noreply.github.com>
2024-04-19 07:16:32 +0500

    my first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// import inquirer module
+import inquirer from "inquirer";
+const answers = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: "Enter your sentence to count the word.",
+    },
+]);
+// using trim and split method for removing white spaces and making an array of words
+const words = answers.Sentence.trim().split(" ");
+// print the array of words to console
+console.log(words);
+console.log(`Your sentence word count is ${words.length}`);
